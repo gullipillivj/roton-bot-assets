55142713f2731e522d0a1300a011ef3ddca15c4b
Vijay bhaskar gullipilli <gullipillivijaybhaskar@gmail.com>
2026-06-30 12:05:20 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,5 @@
 async function initBot(userId) {
-  debugLog("main", ">>> Entered initBot for " + userId);
-
-  // Minimal test
-  if (typeof runBot === "function") {
-    debugLog("main", "runBot is defined, calling now");
-    await runBot(userId);
-  } else {
-    debugError("main", "runBot not found");
-  }
-
-  debugLog("main", "<<< Exiting initBot");
+  debugLog("main", "initBot good for " + userId);
 }
 
 window.initBot = initBot;
