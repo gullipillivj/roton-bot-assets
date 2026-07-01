41a477fece8bc0503a26126cb3499f60d27ea4e3
Vijay bhaskar gullipilli <gullipillivijaybhaskar@gmail.com>
2026-07-01 08:03:40 +0530

    Delete debug.js

--- debug.js
+++
@@ -1,9 +0,0 @@
-function debugLog(module, message, extra) {
-  console.log(`[${module}] ${message}`, extra || "");
-}
-function debugError(module, err) {
-  console.error(`[${module}] ERROR:`, err);
-}
-
-window.debugLog = debugLog;
-window.debugError = debugError;
