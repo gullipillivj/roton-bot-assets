a51262bee36227408417987d82854d646c98350e
Vijay bhaskar gullipilli <gullipillivijaybhaskar@gmail.com>
2026-07-01 07:14:29 +0530

    Remove debug logging from trade history functions

--- updateHistory.js
+++ updateHistory.js
@@ -3,19 +3,17 @@
 async function logTrade(userId, date, tradeData) {
     try {
         await firebase.add('trade_history', { userId, date, ...tradeData });
-        debugLog("updateHistory", `Trade logged for user ${userId}`, tradeData);
-    } catch (err) {
-        debugError("updateHistory", err);
+        } catch (err) {
+        
     }
 }
 
 async function getHistory(date) {
     try {
         const history = await firebase.getAll('trade_history', { date });
-        debugLog("updateHistory", `Fetched history for ${date}`, history);
-        return history;
+          return history;
     } catch (err) {
-        debugError("updateHistory", err);
+        
         return [];
     }
 }
@@ -23,10 +21,8 @@ async function getHistory(date) {
 async function wipeHistory() {
     try {
         await firebase.clear('trade_history');
-        debugLog("updateHistory", "Trade history wiped for new date");
-    } catch (err) {
-        debugError("updateHistory", err);
-    }
+         } catch (err) {
+         }
 }
 
 
